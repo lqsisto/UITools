0140e3bd4db48316ccc5f0ebffee13115798a42c
lqsisto <36103237+lqsisto@users.noreply.github.com>
2021-06-02 09:51:36 +0300

    Completely rewrite Buttonframe component

---
+++ Assets/Editor/ButtonFrameEditor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Components;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using Editor;
+using UnityEditor.UI;
+
+namespace Editor
+{
+    [CustomEditor(typeof(ButtonFrame))]
+    public class ButtonFrameEditor : HorizontalOrVerticalLayoutGroupEditor
+    {
+        private SerializedProperty modifiedButtonAmount;
+        private SerializedProperty buttonPrefab;
+
+        private SerializedProperty isVerticalLayoutGroup;
+
+        private ButtonFrame buttonFrame;
+
+        public override void OnInspectorGUI()
+        {
+            //Find values that are modified
+            modifiedButtonAmount = serializedObject.FindProperty("modifiedButtonAmount");
+            isVerticalLayoutGroup = serializedObject.FindProperty("isVerticalLayoutGroup");
+            buttonPrefab = serializedObject.FindProperty("buttonPrefab");
+
+            buttonFrame = (ButtonFrame)target;
+
+            EditorGUILayout.PropertyField(buttonPrefab);
+            EditorGUILayout.BeginHorizontal();
+            EditorGUI.BeginChangeCheck();
+            if (GUILayout.Button("-"))
+            {
+                if (modifiedButtonAmount.intValue > 0)
+                {
+                    modifiedButtonAmount.intValue--;
+                    serializedObject.ApplyModifiedProperties();
+                    buttonFrame.InstantiateButtons();
+                }
+
+
+            }
+
+            EditorGUILayout.PropertyField(modifiedButtonAmount, GUIContent.none);
+
+            if (GUILayout.Button("+"))
+            {
+                modifiedButtonAmount.intValue++;
+                serializedObject.ApplyModifiedProperties();
+                buttonFrame.InstantiateButtons();
+            }
+
+            EditorGUI.EndChangeCheck();
+            EditorGUILayout.EndHorizontal();
+
+
+            var btnFrameGo = buttonFrame.gameObject;
+
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Vertical Group", GUILayout.Height(50)))
+            {
+                EditorGUI.BeginChangeCheck();
+
+                isVerticalLayoutGroup.boolValue = true;
+
+                EditorGUI.EndChangeCheck();
+
+            }
+
+            if (GUILayout.Button("Horizontal Group", GUILayout.Height(50)))
+            {
+                EditorGUI.BeginChangeCheck();
+                isVerticalLayoutGroup.boolValue = false;
+
+                EditorGUI.EndChangeCheck();
+            }
+
+            serializedObject.ApplyModifiedProperties();
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ Assets/Runtime/ButtonFrame.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Components
+{
+    public class ButtonFrame : HorizontalOrVerticalLayoutGroup
+    {
+        [SerializeField] private int modifiedButtonAmount;
+        [SerializeField] protected int currentButtonAmount;
+
+#pragma warning disable CS0649 
+        [SerializeField] private bool isVerticalLayoutGroup;
+#pragma warning restore CS0649 
+
+        [SerializeField] protected Button buttonPrefab;
+
+        private RectTransform attachedRectTransform;
+
+        public List<Button> instantiatedButtons = new List<Button>();
+        
+
+        protected override void Reset()
+        {
+            base.Reset();
+
+            attachedRectTransform = GetComponent<RectTransform>();
+            currentButtonAmount = transform.childCount;
+            modifiedButtonAmount = currentButtonAmount;
+
+            m_ChildAlignment = TextAnchor.MiddleCenter;
+
+            childControlWidth = false;
+            childControlHeight = false;
+
+
+            if (transform.childCount > 0)
+            {
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    instantiatedButtons.Add(transform.GetChild(i).GetComponent<Button>());
+                }
+            }
+
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            EditorApplication.delayCall += SetRectSizeToMatchContentSize;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            EditorApplication.delayCall -= SetRectSizeToMatchContentSize;
+        }
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            SetRectSizeToMatchContentSize();
+
+            if (!transform.parent.GetComponent<Canvas>())
+            {
+                throw new MissingComponentException("Parent Gameobject does not have canvas!");
+            }
+            if (transform.parent == null)
+            {
+                throw new UnityException("Gameobject has no parent");
+            }
+
+            //TODO only check children that have button component
+            currentButtonAmount = transform.childCount;
+
+        }
+
+        public void SetRectSizeToMatchContentSize()
+        {
+            Internal_SetRectSizeToMatchContentSize();
+        }
+
+        private void Internal_SetRectSizeToMatchContentSize()
+        {
+
+            if(!attachedRectTransform)
+            {
+                return;
+            }
+
+
+            if (isVerticalLayoutGroup)
+            {
+                var height = 0f;
+                var widest = 0f;
+
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    var c = transform.GetChild(i);
+
+                    if (c.GetComponent<RectTransform>())
+                    {
+                        var cr = c.GetComponent<RectTransform>();
+                        height += cr.rect.height;
+
+                        if(cr.rect.width > widest)
+                        {
+                            widest = cr.rect.width;
+                        }
+                    }
+                }
+                height += (m_Spacing * transform.childCount) + m_Padding.top + m_Padding.bottom;
+                widest += m_Padding.left + m_Padding.right;
+                attachedRectTransform.sizeDelta = new Vector2(widest, height);
+            }
+            else
+            {
+                var width = 0f;
+                var highest = 0f;
+
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    var c = transform.GetChild(i);
+
+                    if (c.GetComponent<RectTransform>())
+                    {
+                        var cr = c.GetComponent<RectTransform>();
+                        width += cr.rect.width;
+
+                        if(cr.rect.height > highest)
+                        {
+                            highest = cr.rect.height;
+                        }
+                    }
+                }
+                width += (m_Spacing * transform.childCount) + m_Padding.left + m_Padding.right;
+                highest += m_Padding.top + m_Padding.bottom;
+                attachedRectTransform.sizeDelta = new Vector2(width, highest);
+            }
+        }
+        public virtual void InstantiateButtons()
+        {
+            if (modifiedButtonAmount == currentButtonAmount)
+                return;
+
+            //if button amount is less when checked last
+            if (modifiedButtonAmount < currentButtonAmount)
+            {
+                StartCoroutine(Delete());
+            }
+            else
+            {
+                var buttonToInstantiate = modifiedButtonAmount - currentButtonAmount;
+
+                if (buttonToInstantiate < 0)
+                    buttonToInstantiate = 0;
+
+                for (var i = 0; i < buttonToInstantiate; i++)
+                {
+                    var button = Instantiate(buttonPrefab, transform);
+                    Undo.RegisterCreatedObjectUndo(button, "Created go");
+                    button.name = "ButtonPrefab";
+                    instantiatedButtons.Add(button);
+                }
+
+                instantiatedButtons.RemoveAll(item => item == null);
+                currentButtonAmount = modifiedButtonAmount;
+
+                Internal_SetRectSizeToMatchContentSize();
+            }
+        }
+
+        private IEnumerator Delete()
+        {
+            var buttonsToRemove = currentButtonAmount - modifiedButtonAmount;
+
+            for (int i = instantiatedButtons.Count - 1; i > (instantiatedButtons.Count - 1) - buttonsToRemove; i--)
+            {
+                Undo.DestroyObjectImmediate(instantiatedButtons[i]);
+                yield return null;
+            }
+            instantiatedButtons.RemoveAll(item => item == null);
+            currentButtonAmount = modifiedButtonAmount;
+            Internal_SetRectSizeToMatchContentSize();
+        }
+
+        public override void CalculateLayoutInputHorizontal()
+        {
+            base.CalculateLayoutInputHorizontal();
+            CalcAlongAxis(0, isVerticalLayoutGroup);
+        }
+
+        public override void CalculateLayoutInputVertical()
+        {
+            CalcAlongAxis(1, isVerticalLayoutGroup);
+        }
+
+        public override void SetLayoutHorizontal()
+        {
+            SetChildrenAlongAxis(0, isVerticalLayoutGroup);
+        }
+
+        public override void SetLayoutVertical()
+        {
+            SetChildrenAlongAxis(1, isVerticalLayoutGroup);
+        }
+    }
+}
